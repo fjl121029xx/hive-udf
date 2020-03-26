c2537780b214670e78399475006a20e97ef8e0ee
fjl121029xx <sishengqikuo_xx@163.com>
2020-03-26 18:34:42 +0800

    20200326   ...1

---
+++ src/main/java/com/hll/udf/dif/UDFHourDiff.java
@@ -0,0 +1,92 @@
+package com.hll.udf.dif;
+
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.*;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
+@Description(name = "udfhourdiff",
+        value = "_FUNC_(start, end) - " +
+                "date:Timestamp , day:Timestamp")
+public class UDFHourDiff extends GenericUDF {
+
+    private TimestampObjectInspector dateObjectInspector01;
+    private TimestampObjectInspector dateObjectInspector02;
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] objectInspectors) throws UDFArgumentException {
+        if (objectInspectors.length != 2) {
+            throw new UDFArgumentLengthException("arrayContainsExample only takes 2 arguments: date,  date");
+        }
+        // 1. 检查是否接收到正确的参数类型
+        ObjectInspector a = objectInspectors[0];
+        ObjectInspector b = objectInspectors[1];
+
+//        if (!(a instanceof WritableTimestampObjectInspector) || !(b instanceof WritableTimestampObjectInspector)) {
+//            System.out.println("1 "+a.getClass());
+//            System.out.println("1 "+b.getClass());
+        if (!(a instanceof TimestampObjectInspector) || !(b instanceof TimestampObjectInspector)) {
+            System.out.println("2 " + a.getClass());
+            System.out.println("2 " + b.getClass());
+            throw new UDFArgumentException(String.format("first argument must be a Timestamp, second argument must be a Timestamp %s %s", a.getClass(), b.getClass()));
+        }
+//        }
+
+        this.dateObjectInspector01 = (TimestampObjectInspector) a;
+        this.dateObjectInspector02 = (TimestampObjectInspector) b;
+
+        return PrimitiveObjectInspectorFactory.javaDoubleObjectInspector;
+    }
+
+
+    @Override
+    public Object evaluate(DeferredObject[] deferredObjects) throws HiveException {
+
+        Timestamp start = this.dateObjectInspector01.getPrimitiveJavaObject(deferredObjects[0].get());
+        Timestamp end = this.dateObjectInspector02.getPrimitiveJavaObject(deferredObjects[1].get());
+        if (start == null || end == null) {
+            throw new UDFArgumentLengthException(String.format("args has null :=} start is %s end is %s", start, end));
+        }
+        Calendar startCa = Calendar.getInstance();
+        startCa.setTime(new Date(start.getTime()));
+        Calendar endCa = Calendar.getInstance();
+        endCa.setTime(new Date(end.getTime()));
+
+        System.out.println("start " + start);
+        System.out.println("end " + end);
+        System.out.println("startCa " + startCa);
+        System.out.println("endCa " + endCa);
+        System.out.println("startCa.getTime().getTime() " + startCa.getTime().getTime());
+        System.out.println("endCa.getTime().getTime() " + endCa.getTime().getTime());
+        BigDecimal startSecond = new BigDecimal(startCa.getTime().getTime());
+        BigDecimal endSecond = new BigDecimal(endCa.getTime().getTime());
+        BigDecimal div = new BigDecimal(60 * 60);
+        try {
+            System.out.println(startSecond);
+            System.out.println(endSecond);
+            System.out.println(div);
+            BigDecimal result = endSecond.subtract(startSecond).divide(div).setScale(2, BigDecimal.ROUND_UP);
+            return result.doubleValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(String.format("math start %s end %s div %s", startSecond.toString(), endSecond, div));
+        }
+
+    }
+
+    @Override
+    public String getDisplayString(String[] children) {
+        return children[0];
+    }
+
+
+}
---
+++ src/main/java/com/hll/udf/dif/UDFWeekDiff.java
@@ -0,0 +1,60 @@
+package com.hll.udf.dif;
+
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.DateObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.StringObjectInspector;
+
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
+
+public class UDFWeekDiff extends GenericUDF {
+    private DateObjectInspector dateObjectInspector01;
+    private DateObjectInspector dateObjectInspector02;
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] objectInspectors) throws UDFArgumentException {
+        if (objectInspectors.length != 2) {
+            throw new UDFArgumentLengthException("arrayContainsExample only takes 2 arguments: date,  date");
+        }
+        // 1. 检查是否接收到正确的参数类型
+        ObjectInspector a = objectInspectors[0];
+        ObjectInspector b = objectInspectors[1];
+        if (!(a instanceof DateObjectInspector) || !(b instanceof DateObjectInspector)) {
+            throw new UDFArgumentException("first argument must be a String, second argument must be a String");
+        }
+
+        this.dateObjectInspector01 = (DateObjectInspector) a;
+        this.dateObjectInspector02 = (DateObjectInspector) b;
+
+        return PrimitiveObjectInspectorFactory.javaIntObjectInspector;
+    }
+
+
+    @Override
+    public Object evaluate(DeferredObject[] deferredObjects) throws HiveException {
+
+        Date start = this.dateObjectInspector01.getPrimitiveJavaObject(deferredObjects[0].get());
+        Date end = this.dateObjectInspector02.getPrimitiveJavaObject(deferredObjects[1].get());
+
+        if (start == null || end == null) {
+            throw new UDFArgumentLengthException(String.format("args has null :=} start is %s end is %s", start, end));
+        }
+
+        LocalDate startDate = LocalDate.of(start.getYear(), start.getMonth(), start.getDay());
+        LocalDate endDate = LocalDate.of(end.getYear(), end.getMonth(), end.getDay());
+
+        return ChronoUnit.WEEKS.between(startDate, endDate);
+    }
+
+    @Override
+    public String getDisplayString(String[] children) {
+        return children[0];
+    }
+}
